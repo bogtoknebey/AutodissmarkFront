02e081e0b5e932ac565063b1f374e5f6247d5e8c
Valerii Yaremenko <v.yaremenko@andersenlab.com>
2024-07-22 02:59:12 +0200

    first commit

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    //baseUrl: 'https://localhost:5001/api',
+    baseUrl: 'https://localhost:7248/api',
+    //baseUrl: 'https://back.poetry.realhost-free.net/api',
+    author: "Mark"
+};
